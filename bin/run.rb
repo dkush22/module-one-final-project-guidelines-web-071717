0a841825d54fca31e5aa877c77454c6be966080d
dkush22 <dkush22@gmail.com>
2017-07-31 17:27:08 -0400

    populated exchanges and stocks via csv

--- bin/run.rb
+++ bin/run.rb
@@ -1,10 +1,43 @@
 require_relative '../config/environment.rb'
+require 'csv'
+ 
 
-daniel = Investor.create(name: "Daniel")
-luke = Investor.create(name: "Luke")
-nyse = Exchange.create(name: "NYSE")
-nasdaq = Exchange.create(name: "NASDAQ")
-apple = Stock.create(name: "apple", sector: "technology", ticker: "AAPL")
-mmm = Stock.create(name: "3M", sector: "industrials", ticker: "3M")
+
+# daniel = Investor.create(name: "Daniel")
+# luke = Investor.create(name: "Luke")
+
+
+exchanges = []
+exchange_path = 'bin/exchange.csv'
+
+CSV.foreach(exchange_path) do |row|
+    # Where row[i] corresponds to a zero-based value/column in the csv
+    Exchange.create(name: row[1])
+    exchanges << row[1]
+end
+
+
+# puts exchanges
+
+stocks = []
+stocks_path = 'bin/nyse.csv'
+stocks_path2 = 'bin/nasdaq.csv'
+
+
+CSV.foreach(stocks_path) do |row|
+    # Where row[i] corresponds to a zero-based value/column in the csv
+    stocks << row[1]
+	Stock.create(exchange_id: 1, name: row[1], ticker: row[0], sector: row[3])
+end
+
+CSV.foreach(stocks_path2) do |row|
+    # Where row[i] corresponds to a zero-based value/column in the csv
+    Stock.create(exchange_id: 2, name: row[1], ticker: row[0], sector: row[4])
+    stocks << row[1]
+end
 
 binding.pry
+
+
+
+# puts stocks
