8c0f1a252427ca24bbb5b3d660d9cdd4e1d8f06e
Gianpaul Rachiele <gianpaul817@gmail.com>
2017-08-01 11:29:22 -0400

    This is the most recent work

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,7 @@ require 'bundler'
 Bundler.require
 require_all 'app'
 require 'pry'
+require 'date'
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
 require_all 'lib'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 4) do
+ActiveRecord::Schema.define(version: 5) do
 
   create_table "people", force: :cascade do |t|
     t.string "name"
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 4) do
   create_table "todos", force: :cascade do |t|
     t.string   "name"
     t.datetime "due_date"
-    t.string   "action"
+    t.string   "description"
     t.string   "status"
     t.datetime "created_at"
     t.datetime "updated_at"
