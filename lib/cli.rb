0cd125a3cdf914910f373613bf32b2251c070ca2
dkush22 <dkush22@gmail.com>
2017-08-01 14:31:19 -0400

    added more user input and stock question functionality

--- lib/cli.rb
+++ lib/cli.rb
@@ -3,7 +3,7 @@ def greet
 end
 
 def gets_user_input
-    puts "Find out more information about an Exchange, a Stock, or an Investor. Enter 'Exchange', 'Stock', or 'Investor' to find out more"
+    puts "Find out more information about an Exchange or a Stock. Enter 'Exchange' or 'Stock' to find out more"
 
     input = gets.chomp.downcase
 
@@ -13,20 +13,15 @@ def gets_user_input
         return input 
     elsif input == "exchange"
         return input
-    elsif input == 'investor'
-        return input
     end
 
     until cond == true
-        puts "Unexpected input. Please enter 'Exchange', 'Stock', or 'Investor' to research their data"
+        puts "Unexpected input. Please enter 'Exchange' or 'Stock' to research their data"
         input = gets.chomp.downcase
          if input == "stock"
             cond == true
             return input 
         elsif input == "exchange"
-            cond == true
-            return input
-        elsif input == 'investor'
             cond == true
             return input
         end
@@ -40,25 +35,122 @@ def research_topic(topic)
         input = gets.chomp
         stock_data = search_for_stock(input)
     elsif topic == "exchange"
-        
-    elsif topic == 'investor'
-        
+        puts "You chose exchanges! Enter an exchange name."
+        input = gets.chomp
+        exchange_data = search_for_exchange(input)
     end
 end
 
-def search_for_stock(stock)
-    sql = <<-SQL 
-        SELECT * FROM stocks WHERE name = ? OR ticker = ?
-    SQL 
-    vals = connection.execute(sql, stock, stock)
+def search_for_stock(searchVal)
+     val = Stock.find do  |stock| 
+        stock.ticker.upcase == searchVal || stock.name.include?(searchVal)
+    end
+    val
+end
+
+def search_for_exchange(searchVal)
+    val = Exchange.find do |exchange| 
+        exchange.name.include?(searchVal)
+    end
+    val
+end
+
+def find_exchange(stock)
+    val = Exchange.find do |exchange|
+        exchange.id == stock.exchange_id
+    end
+    val.name
+end
+
+def get_total_investment(stock)
+    val = 0
+    Investment.all.each do |inv|
+        # binding.pry
+        if stock.id == inv.stock_id
+            # binding.pry
+            val += inv.amount
+        end
+    end
+    val
+end
+
+def get_largest_investor(stock)
+    arr = []
+    new_array = []
+
+    Investment.all.each do |inv|
+        if stock.id == inv.stock_id
+            arr << inv
+        end
+    end
+
+    amt_arr = []
+    arr.each do |inv|
+        amt_arr << inv.amount
+    end
+
+    max_val = amt_arr.max 
+
+    max_investment = arr.find do |inv|
+        max_val == inv.amount 
+    end
+
+    val = Investor.find do |investor|
+        max_investment.investor_id == investor.id
+    end
+
+    returnArr = [val.name, max_investment.amount]
+end
+
+def learn_exchange(exchange)
+
+
+end
+
+def learn_stock(stock)
+    name = find_exchange(stock)
+    puts "You selected #{stock.name}. The ticker for this stock is #{stock.ticker}. This stock is on the #{name}!"
+    puts ""
+
+    #Q1
+    puts "Would you like to see the total investment in this stock? 'y' or 'n'"
+    input = gets.chomp.downcase
+    if input == "y"
+        puts "The total invesment of #{stock.name} is $#{get_total_investment(stock)}."
+    end
+
+    #Q2
+     puts "Would you see which person has the most equity in this stock? 'y' or 'n'"
+    input = gets.chomp.downcase
+    if input == "y"
+        arr = get_largest_investor(stock)
+        puts "The biggest investor of #{stock.name} is #{arr[0]} with an invesment of $#{arr[1]}."
+    end
+
+
 end
 
 
+
+
+def goodbye
+    puts "Thank you for using the application! We hope it was useful!"
+end
 
 
 
 def run 
     greet
     topic = gets_user_input
-    research_topic(topic)
+    result = research_topic(topic)
+
+    if result.class == Exchange 
+        learn_exchange(result)
+    elsif result.class == Stock
+        learn_stock(result)
+    end
+
+
+    # goodbye
+
 end
